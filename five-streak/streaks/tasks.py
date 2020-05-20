3bc875098806789d0e2323b398f0819ad68632e2
dolphingarlic <dolphingarlic@gmail.com>
2020-05-20 08:37:20 +0200

    Fixed typo in tasks.py

--- five-streak/streaks/tasks.py
+++ five-streak/streaks/tasks.py
@@ -10,5 +10,5 @@ from .models import Streak
 @periodic_task(run_every=crontab(hour=0, minute=0))
 def clear_inactive_streaks():
     print('Clearing inactive streaks...')
-    Streak.objects.filter(~Q(last_edited=datetime.today()),
+    Streak.objects.filter(~Q(last_updated=datetime.today()),
                           Q(active=True)).update(active=False)
