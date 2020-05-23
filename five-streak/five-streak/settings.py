f87743b02af233f0c1beac52ef1319bf8873a5c0
dolphingarlic <dolphingarlic@gmail.com>
2020-05-23 18:11:50 +0200

    PWA stuff

--- five-streak/five-streak/settings.py
+++ five-streak/five-streak/settings.py
@@ -162,8 +162,16 @@ PWA_APP_ORIENTATION = 'any'
 PWA_APP_START_URL = '/'
 PWA_APP_ICONS = [ { 'src': 'static/frontend/favicon.png', 'sizes': '192x192' } ]
 PWA_APP_ICONS_APPLE = [ { 'src': 'static/frontend/favicon.png', 'sizes': '192x192' } ]
+PWA_APP_SPLASH_SCREEN = [
+    {
+        'src': '/static/frontend/brand.svg',
+        'media': '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)'
+    }
+]
 PWA_APP_DIR = 'ltr'
 PWA_APP_LANG = 'en-US'
+
+PWA_APP_DEBUG_MODE = False
 
 # Configure Django App for Heroku.
 import django_heroku
