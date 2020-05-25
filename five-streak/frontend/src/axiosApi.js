699fd0b9430a089a4c22c835e51e1941991f810e
dolphingarlic <dolphingarlic@gmail.com>
2020-05-25 11:49:53 +0200

    Changed axios baseURL

--- five-streak/frontend/src/axiosApi.js
+++ five-streak/frontend/src/axiosApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "https://five-streak.herokuapp.com/api/";
+const baseURL = "https://five-streak.com/api/";
 
 const axiosInstance = axios.create({
     baseURL: baseURL,
